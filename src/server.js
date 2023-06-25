897f583d09dbdb31d6640146c1b55a242225dcb0
KimJongKyu <hisprit@naver.com>
2023-06-25 13:27:30 +0900

    first f commit

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 // const express = require("express"); // require("express")형식은 CommonJS스타일 임
-
 import express from "express";
+import morgan from "morgan";
+
 const app = express();
 const port = 3000;
+const logger = morgan("combined");
 
-app.get('/', (req, res) => {
-    console.log("================================================")
-    
-    res.send('Hello Express!!!!!!!asdfsdfsda') ;  
-});
+
+const home = (req, res) => {
+    console.log("home");
+    res.send('handle home');
+    // res.end()
+}
+
+app.use(logger);
+app.get('/', home);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port} + 3000!`);
